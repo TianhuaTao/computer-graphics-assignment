82ddf1b54393c510a938d18c13d82c838dae7604
Sam Tao <tth135@126.com>
2020-03-25 22:25:09 +0800

    add PA2

---
+++ PA2.OpenGLMesh/code/build/CMakeFiles/PA2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sam/Downloads/PA2.OpenGLMesh/code/src/camera_controller.cpp" "/Users/sam/Downloads/PA2.OpenGLMesh/code/build/CMakeFiles/PA2.dir/src/camera_controller.cpp.o"
+  "/Users/sam/Downloads/PA2.OpenGLMesh/code/src/image.cpp" "/Users/sam/Downloads/PA2.OpenGLMesh/code/build/CMakeFiles/PA2.dir/src/image.cpp.o"
+  "/Users/sam/Downloads/PA2.OpenGLMesh/code/src/main.cpp" "/Users/sam/Downloads/PA2.OpenGLMesh/code/build/CMakeFiles/PA2.dir/src/main.cpp.o"
+  "/Users/sam/Downloads/PA2.OpenGLMesh/code/src/mesh.cpp" "/Users/sam/Downloads/PA2.OpenGLMesh/code/build/CMakeFiles/PA2.dir/src/mesh.cpp.o"
+  "/Users/sam/Downloads/PA2.OpenGLMesh/code/src/scene_parser.cpp" "/Users/sam/Downloads/PA2.OpenGLMesh/code/build/CMakeFiles/PA2.dir/src/scene_parser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/GLUT.framework/Headers"
+  "../deps/vecmath/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/sam/Downloads/PA2.OpenGLMesh/code/build/deps/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
