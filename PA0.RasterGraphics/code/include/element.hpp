cfdb3c44e4d9e314cec2dfa39d498893fb0959c2
Sam Tao <tth135@126.com>
2020-05-22 00:46:19 +0800

    update

--- PA0.RasterGraphics/code/include/element.hpp
+++ PA0.RasterGraphics/code/include/element.hpp
@@ -1,67 +1,91 @@
 #pragma once
 
-#include <image.hpp>
 #include <algorithm>
-#include <queue>
 #include <cstdio>
-#include <algorithm>
+#include <image.hpp>
+#include <iostream>
 #include <queue>
-class Element
-{
+#include <cstdlib>
+class Element {
 public:
     virtual void draw(Image &img) = 0;
     virtual ~Element() = default;
 };
 
-class Line : public Element
-{
+class Line : public Element {
 
 public:
     int xA, yA;
     int xB, yB;
     Vector3f color;
-    void draw(Image &img) override
-    {
-        // TODO: Implement Bresenham Algorithm
+    void draw(Image &img) override {
+        // Implement Bresenham Algorithm
         printf("Draw a line from (%d, %d) to (%d, %d) using color (%f, %f, %f)\n", xA, yA, xB, yB,
                color.x(), color.y(), color.z());
-        if (xA > xB)
-        {
-            std::swap(xA, xB);
-            std::swap(yA, yB);
+        
+        if(std::abs(yB-yA)<std::abs(xB-xA)){
+            if(xA>xB)
+                drawLineLow(xB, yB, xA, yA,img);
+            else
+                drawLineLow(xA, yA, xB, yB, img);
+
+        } else {
+            if (yA > yB)
+                drawLineHigh(xB, yB, xA, yA, img);
+            else
+                drawLineHigh(xA, yA, xB, yB, img);
         }
-        int x, y, dx, dy;
-        float k, e;
-        dx = xB - xA;
-        dy = yB - yA;
-        k = dy / dx;
-        e = -0.5;
-        x = xA;
-        y = yA;
-        for (int i = 0; i <= dx; i++)
-        {
-            img.SetPixel(x, y, color);
-            x++;
-            e += k;
-            if (e >= 0)
-            {
-                y++;
-                e -= 1;
+        
+    }
+    private:
+        void drawLineLow(int x0, int y0, int x1, int y1, Image &img) {
+            int dx = x1 - x0;
+            int dy = y1 - y0;
+            int yy = 1;
+            if (dy < 0) {
+                yy = -1;
+                dy = -dy;
+            }
+            int e = 2 * dy - dx;
+            int y = y0;
+            for (int x = x0; x <= x1; x++) {
+                img.SetPixel(x,y,color);
+                if(e>0){
+                    y = y + yy;
+                    e = e - 2 * dx;
+                }
+                e = e + 2 * dy;
             }
         }
-    }
+        void drawLineHigh(int x0, int y0, int x1, int y1, Image &img) {
+            int dx = x1 - x0;
+            int dy = y1 - y0;
+            int xx = 1;
+            if (dx < 0) {
+                xx = -1;
+                dx = -dx;
+            }
+            int e = 2 * dx - dy;
+            int x = x0;
+            for (int y = y0; y <= y1; y++) {
+                img.SetPixel(x, y, color);
+                if (e > 0) {
+                    x = x + xx;
+                    e = e - 2 * dy;
+                }
+                e = e + 2 * dx;
+            }
+        }
 };
 
-class Circle : public Element
-{
+class Circle : public Element {
 
 public:
     int cx, cy;
     int radius;
     Vector3f color;
-    void draw(Image &img) override
-    {
-        // TODO: Implement Algorithm to draw a Circle
+    void draw(Image &img) override {
+        // Implement Algorithm to draw a Circle
         printf("Draw a circle with center (%d, %d) and radius %d using color (%f, %f, %f)\n", cx, cy, radius,
                color.x(), color.y(), color.z());
         int x, y;
@@ -70,12 +94,10 @@ public:
         y = radius;
         d = 1.25 - radius;
         circlePoints(img, x, y, color);
-        while (x <= y)
-        {
+        while (x <= y) {
             if (d < 0)
                 d += 2 * x + 3;
-            else
-            {
+            else {
                 d += 2 * (x - y) + 5;
                 y--;
             }
@@ -85,8 +107,7 @@ public:
     }
 
 private:
-    void circlePoints(Image &img, int x, int y, Vector3f color)
-    {
+    void circlePoints(Image &img, int x, int y, Vector3f color) {
         img.SetPixel(cx + x, cy + y, color);
         img.SetPixel(cx + y, cy + x, color);
         img.SetPixel(cx - x, cy + y, color);
@@ -98,37 +119,47 @@ private:
     }
 };
 
-class Fill : public Element
-{
+class Fill : public Element {
 
 public:
     int cx, cy;
     Vector3f color;
-    void draw(Image &img) override
-    {
-        // TODO: Flood fill
+    void draw(Image &img) override {
+        // Flood fill
         printf("Flood fill source point = (%d, %d) using color (%f, %f, %f)\n", cx, cy,
                color.x(), color.y(), color.z());
         int x = cx;
         int y = cy;
         Vector3f old_color = img.GetPixel(cx, cy);
+        if (old_color == color)
+            return;
+        int width = img.Width();
+        int height = img.Height();
         std::queue<std::pair<int, int>> q;
         q.push(std::make_pair(cx, cy));
-        do
-        {
+        img.SetPixel(x, y, color);
+        do {
             auto p = q.front();
+            q.pop();
             x = p.first;
             y = p.second;
-            if (img.GetPixel(x, y + 1) == old_color)
+            // std::cout << x << " " << y << std::endl;
+            if (y < height - 1 && img.GetPixel(x, y + 1) == old_color) {
                 q.push(std::make_pair(x, y + 1));
-            if (img.GetPixel(x, y - 1) == old_color)
+                img.SetPixel(x, y + 1, color);
+            }
+            if (y > 0 && img.GetPixel(x, y - 1) == old_color) {
                 q.push(std::make_pair(x, y - 1));
-            if (img.GetPixel(x + 1, y) == old_color)
+                img.SetPixel(x, y - 1, color);
+            }
+            if (x < width - 1 && img.GetPixel(x + 1, y) == old_color) {
                 q.push(std::make_pair(x + 1, y));
-            if (img.GetPixel(x - 1, y) == old_color)
+                img.SetPixel(x + 1, y, color);
+            }
+            if (x > 0 && img.GetPixel(x - 1, y) == old_color) {
                 q.push(std::make_pair(x - 1, y));
-            img.SetPixel(x, y, color);
+                img.SetPixel(x - 1, y, color);
+            }
         } while (!q.empty());
     }
-
 };
